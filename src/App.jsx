46417b3fda518523e9d3d88ab16edda31871eb7f
Nipaaaa1 <Frostone224@gmail.com>
2024-01-19 17:02:05 +0700

    Finish Navigation Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
 function App() {
   return (
-    <main>
-      <nav>
-        <img src="/images/logo.svg" alt="Logo" />
-        <ul>
-          <li>Features</li>
-          <li>Company</li>
-          <li>Carrers</li>
-          <li>About</li>
-        </ul>
-        <div>
-          <a href="#">Login</a>
-          <a href="#">Register</a>
+    <main className="h-svh w-full bg-almostWhite px-10 py-6 font-epilogue">
+      <nav className="flex items-center justify-between">
+        <div className="flex text-mediumGray">
+          <img className="mr-16" src="/images/logo.svg" alt="Logo" />
+          <ul className="flex items-center gap-10">
+            <li className="flex items-center gap-2">
+              Features
+              <img
+                className="h-full w-max"
+                src="/images/icon-arrow-down.svg"
+                alt="Arrow Down"
+              />
+            </li>
+            <li className="flex items-center gap-2">
+              Company
+              <img
+                className="h-full w-max"
+                src="/images/icon-arrow-down.svg"
+                alt="Arrow Down"
+              />
+            </li>
+            <li>Carrers</li>
+            <li>About</li>
+          </ul>
+        </div>
+        <div className="flex items-center gap-10">
+          <a className="text-mediumGray" href="#">
+            Login
+          </a>
+          <a
+            className="rounded-xl border-2 border-mediumGray px-6 py-2 text-mediumGray"
+            href="#"
+          >
+            Register
+          </a>
         </div>
       </nav>
       <section>
