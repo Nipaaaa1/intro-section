dc44461b9eb56e56dff067df7eb93ed680ecdd41
Nipaaaa1 <Frostone224@gmail.com>
2024-01-19 16:39:29 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+function App() {
+  return (
+    <main>
+      <nav>
+        <img src="/images/logo.svg" alt="Logo" />
+        <ul>
+          <li>Features</li>
+          <li>Company</li>
+          <li>Carrers</li>
+          <li>About</li>
+        </ul>
+        <div>
+          <a href="#">Login</a>
+          <a href="#">Register</a>
+        </div>
+      </nav>
+      <section>
+        <div>
+          <div>
+            <h1>Make remote work</h1>
+            <p>
+              Get your team in sync, no matter your location. Streamline
+              processes, create team rituals, and watch productivity soar.
+            </p>
+            <a href="#">Learn More</a>
+          </div>
+          <div>
+            <img src="/images/client-databiz.svg" alt="Databiz" />
+            <img src="/images/client-audiophile.svg" alt="Audiophile" />
+            <img src="/images/client-meet.svg" alt="Meet" />
+            <img src="/images/client-maker.svg" alt="Maker" />
+          </div>
+        </div>
+        <img src="/images/image-hero-desktop.png" alt="Hero" />
+      </section>
+    </main>
+  );
+}
+
+export default App;
